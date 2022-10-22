da7daa993ab41267d06c83a5582176367dfbdb6a
Ryan Bevin <bevybizzle@googlemail.com>
2022-10-22 15:59:23 +0100

    hello world up and running and debugging.

--- Src/main.c
+++ Src/main.c
@@ -2,7 +2,7 @@
 
 static void print_hello (GtkWidget *widget, gpointer data)
 {
-  g_print ("Hello World\n");
+  g_print ("YOU CLICKED ME! AAAAAAAAAAAAHHHHHHHHHHHHHHHHH\n");
 }
 
 static void activate (GtkApplication *app, gpointer user_data)
@@ -12,9 +12,9 @@ static void activate (GtkApplication *app, gpointer user_data)
 
   window = gtk_application_window_new (app);
   gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_default_size (GTK_WINDOW (window), 64, 64);
 
-  button = gtk_button_new_with_label ("Hello World");
+  button = gtk_button_new_with_label ("Click Me");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
   gtk_window_set_child (GTK_WINDOW (window), button);
 
