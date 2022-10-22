fb8d5808bd08f96a34c11b9b45dc8a9a53d5326d
Ryan Bevin <bevybizzle@googlemail.com>
2022-10-22 16:29:21 +0100

    updated some names

--- Src/main.c
+++ Src/main.c
@@ -11,10 +11,11 @@ static void activate (GtkApplication *app, gpointer user_data)
   GtkWidget *button;
 
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 64, 64);
+  gtk_window_set_title (GTK_WINDOW (window), "Installation Helper Wizard");
+  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
 
   button = gtk_button_new_with_label ("Click Me");
+  gtk_icon_view_set_item_width(GTK_WINDOW(window), 20);
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
   gtk_window_set_child (GTK_WINDOW (window), button);
 
@@ -26,7 +27,7 @@ int main (int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new (NULL, G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
